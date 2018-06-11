8ba4c732b1eb8fd11833a381f8f9a82a1d93bc6f
kahboom <rachelyordan@gmail.com>
2018-06-11 17:41:36 +0100

    Update to reference Apicurio in module and component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ApiEditorComponent, EditableApiDefinition } from 'apicurio-design-studio';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent {
+  @ViewChild('apiEditor')
+  apiEditor: ApiEditorComponent;
   title = 'app';
+
+  constructor() {
+    this.apiEditor.api = new EditableApiDefinition();
+  };
 }
