4bdb19382cf697cf5c44f18f8c03c79edc115c1f
Eric Wittmann <eric.wittmann@gmail.com>
2018-06-12 10:45:51 -0400

    make the content not empty

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,5 +13,75 @@ export class AppComponent {
 
   constructor() {
     // Something will be here soon
-  };
+  }
+
+  public apiDefinition(): ApiDefinition {
+    let def: ApiDefinition = new ApiDefinition();
+    def.createdBy = 'user1';
+    def.createdOn = new Date();
+    def.tags = [];
+    def.description = '';
+    def.id = 'api-1';
+    def.spec = {
+      'openapi': '3.0.0',
+      'info': {
+        'title': 'Simple OAI 3.0.0 API',
+        'description': 'A simple API using OpenAPI 3.0.0.',
+        'contact': {
+          'name': 'Example Org',
+          'url': 'http://www.example.org',
+          'email': 'contact@example.org'
+        },
+        'license': {
+          'name': 'Apache 2.0',
+          'url': 'https://www.apache.org/licenses/LICENSE-2.0'
+        },
+        'version': '2.0.11'
+      },
+      'paths': {
+      },
+      'components': {
+        'schemas': {
+          'Address': {
+            'properties': {
+              'name': {},
+              'street': {},
+              'city': {},
+              'state': {},
+              'zip': {}
+            }
+          },
+          'User': {
+            'properties': {
+              'address': {
+                '$ref': '#/components/schemas/Address'
+              }
+            }
+          }
+        },
+        'securitySchemes': {
+          'Basic': {
+            'type': 'http',
+            'description': 'Basic auth.',
+            'scheme': 'Basic'
+          }
+        }
+      },
+      'tags': [
+        {
+          'name': 'foo',
+          'description': 'The Foo tag.'
+        },
+        {
+          'name': 'bar',
+          'description': 'The bar tag.'
+        },
+        {
+          'name': 'baz',
+          'description': 'The baz tag.\n'
+        }
+      ]
+    };
+    return def;
+  }
 }
